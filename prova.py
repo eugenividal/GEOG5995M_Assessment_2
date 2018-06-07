d192486365689d540926647b6f2869679eb94381
Eugeni <32644567+eugenividal@users.noreply.github.com>
2018-06-07 10:22:06 +0100

    Last changes

---
+++ prova.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 14:55:25 2018
+
+@author: eugenividal
+"""
+
+drunk = 10
+print (drunk)
+
+while drunk < 251:
+    print(drunk)
+    drunk += 10
